c75da7f81b567351cf3ed33ee4e4924c2a53b9ac
Gideon <gideon.tobing@gmail.com>
2025-04-28 22:06:55 +1000

    finihsing up

--- app/src/main/java/com/gideon/coursera_little_lemon/MainActivity.kt
+++ app/src/main/java/com/gideon/coursera_little_lemon/MainActivity.kt
@@ -4,13 +4,68 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gideon.coursera_little_lemon.ui.theme.LittleLemonAnimationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            LittleLemonAnimationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxWidth(),
+                    color = MaterialTheme.colorScheme.background
+                ) { Greeting("Android") }
+            }
         }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    var visible by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding() // <-- Add this!
+    ) {
+        Button(onClick = { visible = !visible }) {
+            Text("Button")
+        }
+        AnimatedVisibility(
+            visible = visible,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            Text(text = "Hello, world!")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LittleLemonAnimationTheme {
+        Greeting("Android")
     }
 }
